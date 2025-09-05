cb111a4d0576d04098cc425281868cf733199815
HuaTsai <huatsai42@gmail.com>
2025-09-05 17:32:05 +0800

    init

--- src/main.cc
+++ src/main.cc
@@ -2,8 +2,8 @@
 #include <myproject/math_utils.hpp>
 
 int main() {
-  int result1 = add(3, 4);
-  int result2 = square(5);
-  std::cout << "3 + 4 = " << result1 << std::endl;
-  std::cout << "5 squared = " << result2 << std::endl;
+  const int result1 = add(3, 4);
+  const int result2 = square(5);
+  std::cout << "3 + 4 = " << result1 << "\n";
+  std::cout << "5 squared = " << result2 << "\n";
 }
